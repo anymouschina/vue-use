77182990d81eb62afae1dccc990f9d228ed0ded8
anymouschina <624144061@qq.com>
2018-08-24 16:33:02 +0800

    shopping vue

---
+++ vue-bus/views/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        随机增加：
+      <Counter :number="number"></Counter>
+
+    </div>
+</template>
+<script>
+import Counter from './counter.vue';
+    export default {
+  //注意此处通过VUEX的功能调用store里state的count
+    components:{
+        Counter
+    },
+    data(){
+        return{
+            number:0
+        }
+    },
+    created(){
+        this.$bus.on('add',num=>{
+            this.number+=num
+        });
+    }
+ 
+    }
+</script>
